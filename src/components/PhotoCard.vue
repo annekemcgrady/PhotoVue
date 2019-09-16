beb6bc1e6d754a743cbe84120c02876a33b1a69d
Anneke McGrady <annekemcgrady@gmail.com>
2019-09-16 17:07:41 -0600

    Setup the PhotoContainers and PhotoCard components

---
+++ src/components/PhotoCard.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="photo-card" >
+      <h3>This is the PhotoCard</h3>
+      <p>{{photo.title}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PhotoCard',
+  props: ['photo']
+}
+</script>
+
+<style scoped>
+.photo-card {
+  height: 200px;
+  width: 200px;
+  border: 1px solid black;
+  margin: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to PhotoVue</h1>
+    <PhotosContainer v-bind:photos="photos"/>
   </div>
 </template>
 
 <script>
+
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PhotosContainer from '@/components/PhotosContainer.vue'
+import axios from 'axios'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
+    PhotosContainer
+  }, 
+  data() {
+    return {
+      photos: [
+        {id: 1, title: "Photo One", url: "fakeurl"},
+        {id: 2, title: "Photo Two", url: "fakeurl2"},
+        {id: 3, title: "Photo Three", url: "fakeurl3"}
+        ]
+      }
+    }, 
+  //   created() {
+  //   axios.get('')
+  //   .then(res => this.photos = res.data)
+  //   .catch(error => console.log(error))
+  // }
 }
+
 </script>
